5bb8ff1f2ccc1c65342b07fc5b316c5bdb220359
Wrecker1023 <wrecker1023@gmail.com>
2023-02-21 21:58:30 -0500

    fixed prototypes

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -55,3 +55,15 @@ test('checks if player is alive or not',() => {
     expect(player.isAlive()).toBeFalsy();
 });
 
+test("subtracts form the players health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
